ee4b3762c1dcb147bb68beb40997eb832832283a
Mark Xu <pin.xu@centurygame.com>
2025-09-18 19:40:39 +0800

    testing

---
+++ bitmap_test.go
@@ -0,0 +1,27 @@
+package bitmap_test
+
+import (
+	"testing"
+
+	"github.com/xupin/bitmap/bitmap"
+)
+
+func TestBitMap(t *testing.T) {
+	bm := bitmap.NewBitMap(8)
+
+	t.Logf("初始位图: %08b", bm.GetBits())
+
+	// 测试 Add
+	bm.Add(8)
+	t.Logf("Add(8) 后位图: %08b", bm.GetBits())
+	if !bm.Lookup(8) {
+		t.Errorf("Add(8) 后 Lookup(8) 应该为 true")
+	}
+
+	// 测试 Del
+	bm.Del(8)
+	t.Logf("Del(8) 后位图: %08b", bm.GetBits())
+	if bm.Lookup(8) {
+		t.Errorf("Del(8) 后 Lookup(8) 应该为 false")
+	}
+}
